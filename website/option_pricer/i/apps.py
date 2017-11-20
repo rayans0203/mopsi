c3800b3da1dc8d6025b7cd38e5726829acdebfa0
Rayan <rayans0203@gmail.com>
2017-11-20 15:15:58 +0100

    remove option_pricer folder

--- website/option_pricer/i/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IConfig(AppConfig):
-    name = 'i'
--- website/option_pricer/i/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def home(request):
-	text = """ <h1> Bienvenue sur mon site ! </h1>
-				<p> Ceci deviendra un pricer d'options bientôt ! </p>"""
-	return HttpResponse(text)
