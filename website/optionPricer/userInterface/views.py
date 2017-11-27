c393034c67e743af1175a312339a9eeb492a23c2
Rayan <rayans0203@gmail.com>
2017-11-27 12:50:59 +0100

    modified the graphics of the website

--- website/optionPricer/userInterface/views.py
+++ website/optionPricer/userInterface/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-#	return HttpResponse('here are the inputs')
+
 	return  render(request,'userInterface/home.html')
 
 def pricer(request):
