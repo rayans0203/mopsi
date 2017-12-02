eaabf323d662f0413e535245e4946bb180f955aa
Rayan <rayans0203@gmail.com>
2017-12-02 23:29:48 +0100

    added confidence interval method

--- optionPricer/mlmc.py
+++ optionPricer/mlmc.py
@@ -11,15 +11,15 @@ from time import time
 from matplotlib import pyplot as plt
 import multiprocessing as mp
 
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
+# from sklearn.linear_model import Ridge
+# from sklearn.preprocessing import PolynomialFeatures
+# from sklearn.pipeline import make_pipeline
 
 class MC_simulation():
     def __init__(self,S):
         self.S=S
-    def __call__(self,pricer,W):
-        self.S=pricer.S0*np.exp((pricer.rate-0.5*pricer.vol**2)*pricer.maturity+pricer.vol*np.sqrt(pricer.maturity)*W)
+    def __call__(self,pricer):
+        self.S=pricer.S0*np.exp((pricer.rate-0.5*pricer.vol**2)*pricer.maturity+pricer.vol*np.sqrt(pricer.maturity)*np.random.normal(0,1))
         return self.S
 
 class M1LMC_simulation():
@@ -62,16 +62,15 @@ class optionPricer():
         return S*(1+self.rate*dt+self.vol*W)
     def milstein(self,S,dt,W):
         return S*(1+self.rate*dt+self.vol*W+0.5*(self.vol**2)*(W**2-dt))
-    def mc(self,N,pool,W=None):
-        if W==None:
-            W=np.random.normal(0,1,N)
+    def mc(self,N,pool):
         # on garde les possibilités de paralléliser ou non pour les comparer
         if pool==None:
             S=[]
+            W=np.random.normal(0,1,N)
             for _ in range(N):
                 S.append(self.S0*np.exp((self.rate-0.5*self.vol**2)*self.maturity+self.vol*np.sqrt(self.maturity)*W[_]))
         else:
-            future_res=[pool.apply_async(MC_simulation(0),(self,W[_],)) for _ in range(N)]
+            future_res=[pool.apply_async(MC_simulation(0),(self,)) for _ in range(N)]
             S=[f.get() for f in future_res]
         return S
     def m1lmc(self,N_samples,N_step,pool,schema="euler"):
@@ -79,21 +78,19 @@ class optionPricer():
         if pool==None:
             S=np.ones(N_samples)*s
             dt=self.maturity/N_step
-            for j in range(N_Samples):
+            for j in range(N_samples):
                 W=build_BM(N_step,np.sqrt(dt))
                 if schema=="euler":
-                    for j in range(N_step):
-                        S[j]=self.euler(S[j],dt,W[j])
+                    for _ in range(N_step):
+                        S[j]=self.euler(S[j],dt,W[_])
                 elif schema=="milstein":
-                    for j in range(N_step):
-                        S[j]=self.milstein(S[j],dt,W[j])
+                    for _ in range(N_step):
+                        S[j]=self.milstein(S[j],dt,W[_])
         else:
             future_res=[pool.apply_async(M1LMC_simulation(s),(self,N_step,schema)) for _ in range(N_samples)]
             S=[f.get() for f in future_res]
         return S
     def mlmc(self,n,m=7,schema="euler"):
-        """ for now, we suppose L fixed, then we will implement a version
-        in which we increment L until the mse is greater than a given accuracy"""
         L=int(np.log(n)/np.log(m))
         S=np.zeros(L)
         # l=0
@@ -141,9 +138,10 @@ class optionPricer():
                     P_thin[j]=max(self.strike-S_thin,0)
                     P_coarse[j]=max(self.strike-S_coarse,0)
 
-            Payoff+= np.exp(-self.rate*self.maturity)*(P_thin-P_coarse).mean()
-        return Payoff
-    def price(self,method="mc",pool=None,m=7,N=10**4,n=10**2,schema="euler"):
+            Payoff+= (P_thin-P_coarse).mean()
+        return np.exp(-self.rate*self.maturity)*Payoff
+    def price(self,method="mc",pool=None,m=7,N=10**4,n=10**2,alpha=5,schema="euler"):
+        CI=[]
         if method!='mlmc':
             if method=="bs":
                 return self.BlackScholes()
@@ -155,69 +153,83 @@ class optionPricer():
                 S=self.m1lmc(N,n,pool)
 
             if self.order=="call":
-                payoff=np.mean(list(map(lambda x:max(x-self.strike,0),S)))
+                payoffs=list(map(lambda x:np.exp(-self.rate*self.maturity)*max(x-self.strike,0),S))
+                payoff=np.mean(payoffs)
+                CI=self.confidence_interval(alpha,payoffs)
             elif self.order=="put":
-                payoff=np.mean(list(map(lambda x:max(self.strike-x,0),S)))
+                payoffs=list(map(lambda x:np.exp(-self.rate*self.maturity)*max(x-self.strike,0),S))
+                payoff=np.mean(payoffs)
+                CI=self.confidence_interval(alpha,payoffs)
             else:
                 raise ValueError("'order' muste be either 'call' or 'put'")      
 
             end=time()
-            return (payoff,end-start)
+            return (payoff,end-start,CI)
 
         elif method=="mlmc":
             start=time()
             payoff=self.mlmc(n,m,schema)
             end=time()
-            return (payoff,end-start)
+            return (payoff,end-start,CI)
+
+    def confidence_interval(self,alpha,payoff):
+        n=len(payoff)
+        V=np.std(payoff)**2
+        mu=np.mean(payoff)
+        if self.order=="call":
+            print(mu,ss.norm.cdf(1-alpha/2)*np.sqrt(V/n))
+            return [mu-ss.norm.cdf(1-alpha/2)*np.sqrt(V/n),
+                    mu+ss.norm.cdf(1-alpha/2)*np.sqrt(V/n)]
+        elif self.order=="put":
+            return [mu-ss.norm.cdf(1-alpha/2)*np.sqrt(V/n),
+                    mu+ss.norm.cdf(1-alpha/2)*np.sqrt(V/n)]
+
 
 o=optionPricer(100,100,0.25,1,r=0.05)
+print(o.price())
 
-# Déterminer la relation entre n de MLMC et N de MC pour même temps de calcul
-# les "n" pour MLMC
-X=np.array(range(10,40))
-times=[]
-for x in X:
-    payoff,t=o.price(n=x,method="mlmc")
-    times.append(t)
-# stocke les "N(n)" tels que MC(N(n)) prenne le même temps que MLMC(n)
-print("checkpoint 1")
-y=[]
-N_interval=list(range(300,10**4))
-for t in times:
-    time_N=10**7
-    N_t=0
-    count=0
-    for N in N_interval:
-        payoff,tt=o.price(N=N,method="mc")
-        if (tt-t)**2<(time_N-t)**2:
-            time_N=tt
-            N_t=N
-            count=0
-        else:
-            count+=1
-        if count>10000:
-            break
-        print(N_t, "for ",t)
-    y.append(N_t)
-print('checkpoint 2')
-for degree in [4,5]:
-    print(degree)
-    model=make_pipeline(PolynomialFeatures(degree),Ridge())
-    model.fit(X.reshape(-1,1),y)
+# # Déterminer la relation entre n de MLMC et N de MC pour même temps de calcul
+# # les "n" pour MLMC
+# X=np.array(range(10,50))
+# times=[]
+# for x in X:
+#     payoff,t=o.price(n=x,method="mlmc")
+#     times.append(t)
+# # stocke les "N(n)" tels que MC(N(n)) prenne le même temps que MLMC(n)
+# print("checkpoint 1")
+# y=[]
+# N_interval=list(range(300,2*10**4))
+# for t in times:
+#     time_N=10**7
+#     N_t=0
+#     count=0
+#     for N in N_interval:
+#         payoff,tt=o.price(N=N,method="mc")
+#         if (tt-t)**2<(time_N-t)**2:
+#             time_N=tt
+#             N_t=N
+#             count=0
+#         else:
+#             count+=1
+#         if count>10000:
+#             break
+#     y.append(N_t)
+# print('checkpoint 2')
+# for degree in [4,5]:
+#     print(degree)
+#     model=make_pipeline(PolynomialFeatures(degree),Ridge())
+#     model.fit(X.reshape(-1,1),y)
 
-print(model.predict(10))
-plt.plot(X,y)
-plt.show()
+# y_predict=model.predict(X)
+# from tempfile import TemporaryFile
+# outfile=TemporaryFile()
 
-# obs=o.price(method="bs")
-# (omc,t_mc)=o.price(N=2*10**4,method="mc")
-# (omlmc,t_ml)=o.price(n=(2**0.25)*40,method="mlmc")
-
-
-# print("BS: {} $ vs MLMC: {} $".format(obs,omlmc))
-# print("computation time: {} s \n".format(t_ml))
-# print("BS: {} $ vs MC: {} $".format(obs,omc))
-# print("computation time: {} s".format(t_mc))
+# np.save(outfile,model)
+# plt.plot(X,y,marker='o',label='actual best fit')
+# plt.plot(X,y_predict,marker='>',color='r',label='model prediction')
+# plt.xlabel("Number n of samples used for Euler schematization")
+# plt.ylabel('N(n) of MC simulations')
+# plt.show()
 
 # class MSE_computation():
 #     def __init__(self):
@@ -237,7 +249,7 @@ plt.show()
 #     times=[data[k][1] for k in range(len(data))]
 #     return err/max_iter,np.mean(times)
 
-def mse(ref,pricer,simulations,pool,method="mc",max_iter=100):
+def mse(ref,pricer,simulations,pool,method="mc",max_iter=5):
     err=0
     times=[]
     for _ in range(max_iter):
@@ -247,22 +259,37 @@ def mse(ref,pricer,simulations,pool,method="mc",max_iter=100):
         times.append(t)
     return err/max_iter,np.mean(times)
 
-# a=np.array([10,10**2,10**3,10**4,10**5])
+obs=o.price(method="bs")
+a=np.array([10,10**2,2*10**2,5*10**2,10**3,2*10**3,5*10**3,10**4])
 # pool=mp.Pool(4)
-# outputs=list(map(lambda x: mse(obs,o,x,pool,method="mc"),a))
-# MSE=[outputs[k][0] for k in range(len(outputs))]
-# TIMES=[outputs[k][1] for k in range(len(outputs))]
+# outputs_m1lmc=list(map(lambda x: mse(obs,o,x,pool,method="m1lmc"),a))
+# MSE_m1lmc=[outputs_m1lmc[k][0] for k in range(len(outputs_m1lmc))]
+# TIMES_m1lmc=[outputs_m1lmc[k][1] for k in range(len(outputs_m1lmc))]
 
+# to show the benefits of parallelization
+# outputs_m1lmc_unthreaded=list(map(lambda x: mse(obs,o,x,None,method="m1lmc"),a))
+# MSE_m1lmc_unthreaded=[outputs_m1lmc_unthreaded[k][0] for k in range(len(outputs_m1lmc_unthreaded))]
+# TIMES_m1lmc_unthreaded=[outputs_m1lmc_unthreaded[k][1] for k in range(len(outputs_m1lmc_unthreaded))]
 
-# plt.title('Monte-Carlo simulations using Euler schematization with \n Black-Scholes formula as reference')
-# plt.loglog(np.log(a),MSE,marker='<')
+# outputs_mlmc=list(map(lambda x: mse(obs,o,x,pool,method="mlmc"),a))
+# MSE_mlmc=[outputs_mlmc[k][0] for k in range(len(outputs_mlmc))]
+# TIMES_mlmc=[outputs_mlmc[k][1] for k in range(len(outputs_mlmc))]
+
+# plt.loglog(np.log(a),MSE_mc,marker='<')
+# plt.loglog(np.log(a),MSE_mlmc)
 # plt.grid(True)
 # plt.xlabel("log of the number of samples for MC simulations")
 # plt.ylabel("log of the MSE")
 
-# plt.loglog(np.log(a),TIMES,marker='o')
-# plt.grid(True)
-# plt.xlabel("log of the number of samples for MC simulations")
-# plt.ylabel("log of the computation time")
+# from tempfile import TemporaryFile
+# outfile=TemporaryFile()
+
+# np.save(outfile,(TIMES_m1lmc,TIMES_m1lmc_unthreaded))
+
+# plt.plot(np.log10(a),np.log10(TIMES_m1lmc),marker='o',label="With parallelization")
+# plt.plot(np.log10(a),np.log10(TIMES_m1lmc_unthreaded),marker='<',color='r',label="No parallelization")
+# plt.xlabel("$log_{10}$ of the number of samples for MC simulations \n with Euler schematization")
+# plt.ylabel("$log_{10}$ of the computation time")
+# plt.legend(loc=2)
 # plt.tight_layout()
 # plt.show()
