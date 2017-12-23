8420cd18efde32737fa2ef77c18b1a16c27002e3
lina.faik <lina.faik@eleves.enpc.fr>
2017-12-23 23:29:37 +0100

    message

---
+++ website/optionPricer/userInterface/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+
+	return  render(request,'userInterface/home.html')
+
+def pricer(request):
+
+	return render(request,'userInterface/pricer.html')
+
+def contact(request):
+
+	return render(request,'userInterface/contact.html')
+	
+def p(request):
+	
+	return render(request, 'userInterface/p.py')
