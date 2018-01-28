2406a41d352833491fe414998cf1319223e3d485
Rayan <rayans0203@gmail.com>
2018-01-28 01:55:44 +0100

    changed urlconf for computation.html to take user inputs in arguments

--- website/optionPricer/userInterface/urls.py
+++ website/optionPricer/userInterface/urls.py
@@ -1,15 +1,12 @@
-from django.conf.urls import url,include
-from . import views
 from django.views.generic import ListView, DetailView
 from . import models
+from django.conf.urls import url,include
+from . import views
 
 urlpatterns = [
 	url(r'home/$',views.home,name='home'),
 	url(r'pricer/$',views.pricer,name='pricer'),
-	url(r'contact/$',views.contact,name='contact'),
-	url(r'computation/$',views.computation,name='computation'),
+	url(r'computation/'+r'([0-9]*[.]?[0-9]+)/'*5+r'(\w+)/'*3,views.computation,name="computation"),
 	url(r'faq/$',ListView.as_view(queryset=models.Post.objects.all(),
 		template_name="userInterface/faq.html")),
-	# url(r'(?P<pk>\d+)$',DetailView.as_view(model=models.Post,
-	# 	template_name='userInterface/post.html'))
 ]
