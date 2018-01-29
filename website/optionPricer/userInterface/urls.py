9b3c9e5478529575cb04c5660bb965c27255d96b
lina.faik <lina.faik@eleves.enpc.fr>
2018-01-29 10:08:42 +0100

    message

--- website/optionPricer/userInterface/urls.py
+++ website/optionPricer/userInterface/urls.py
@@ -1,8 +1,14 @@
+from django.views.generic import ListView, DetailView
+from . import models
 from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-	url(r'home$',views.home,name='home'),
-	url(r'pricer$',views.pricer,name='pricer'),
-	url(r'contact$',views.contact,name='contact'),
+	url(r'home/$',views.home,name='home'),
+	url(r'pricer/$',views.pricer,name='pricer'),
+    url(r'contact/$',views.contact,name='contact'),
+	url(r'resources$',views.resources,name='resources'),
+	url(r'computation/'+r'([0-9]*[.]?[0-9]+)/'*5+r'(\w+)/'*3,views.computation,name="computation"),
+	url(r'faq/$',ListView.as_view(queryset=models.Post.objects.all(),
+		template_name="userInterface/faq.html")),
 ]
--- website/optionPricer/userInterface/views.py
+++ website/optionPricer/userInterface/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render,loader
 from django.http import HttpResponse
-from .forms import pricingForm, resultForm
 from . import mlmc
 import json
 
 # Create your views here.
 def home(request):
+
 	return  render(request,'userInterface/home.html')
 
-# https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-django/les-formulaires-6#_=_
 def pricer(request):
-	form = pricingForm(request.POST or None)
-	
-	if form.is_valid(): 
-		Stock = form.cleaned_data['Stock']
-		Model = form.cleaned_data['Model']
-		Strike = form.cleaned_data['Strike']
-		Maturity = form.cleaned_data['Maturity']
-		Volatility = form.cleaned_data['Volatility']
-		Number = form.cleaned_data['Number']
-		Interest = form.cleaned_data['Interest']		
-		o=mlmc.optionPricer(int(Stock),100,0.2,3,"BS","E",kappa=3,rho=0,xi=0.2,theta=.9,v0=0.1)
-		payoff,time=o.price(method="bs")
-				
-	return render(request,'userInterface/pricer.html', locals())
+
+	return render(request,'userInterface/pricer.html')
 
 def contact(request):
+
 	return render(request,'userInterface/contact.html')
+
+def computation(request,S0,K,sigma,T,r,Model,Type,Method):
+	# lorsque l'on clique sur le bouton Calculate, on effectue le calcul
+	S0=float(S0)
+	K=float(K)
+	sigma=float(sigma)
+	T=float(T)
+	r=float(r)
+
+	o=mlmc.optionPricer(S0,K,sigma,T,Model,Type,r=r,kappa=3,rho=0,xi=0.2,theta=.9,v0=0.1)
+	call,time_call=o.price("bs")
+	put,time_put=o.price("bs")
+
+	# rajouter le calcul du delta, gamma et intervalle de confiance
+
+	context={'call':round(call,2),'put':round(put,2)}
+
+	return HttpResponse(json.dumps(context))
+
+
+def resources(request):
+	return render(request,'userInterface/resources.html')
 	
