862894c0271529b7063a2d1be8c90930f77af6f2
Rayan <rayans0203@gmail.com>
2018-02-05 17:54:39 +0100

    corrected bug for asian options + greeks

--- website/optionPricer/userInterface/mlmc.py
+++ website/optionPricer/userInterface/mlmc.py
@@ -11,9 +11,7 @@ from time import time
 from matplotlib import pyplot as plt
 import multiprocessing as mp
 
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
+############### POUR LA PARALLÉLISATION ##############
 
 class MC_simulation():
     def __init__(self,S):
@@ -36,6 +34,7 @@ class M1LMC_simulation():
 ######### UTILS ##############
 
 def build_BM(n,sqrt_dt):
+    """ Construction d'un mouvement brownien """
     W=np.zeros(n+1)
     W_ind=np.random.normal(0,sqrt_dt,n)
     for k in range(n):
@@ -43,7 +42,7 @@ def build_BM(n,sqrt_dt):
     return W
 
 def build_BM_corr(sqrt_dt,rho,W):
-    "returns a brownian motion with correlation 'rho' to input: W"
+    """ Construit un mouvement brownien rho-corrélé à au mouvement brownien W"""
     n=W.shape[0]-1
     W_rho=np.zeros(n+1)
     W_independant=build_BM(n,sqrt_dt)
@@ -53,6 +52,7 @@ def build_BM_corr(sqrt_dt,rho,W):
     return W_rho   
 
 def build_Cox_Ross(W,rho,kappa,theta,xi,dt,pricer):
+    """ Construit un processus de cox-Ross à partir d'un mouvement brownien W"""
     W_rho=build_BM_corr(np.sqrt(dt),rho,W)
     n=W.shape[0]-1
     W_rho_acc=W_rho[1:]-W_rho[:n]
@@ -62,10 +62,10 @@ def build_Cox_Ross(W,rho,kappa,theta,xi,dt,pricer):
             v[k+1]=v[k]+kappa*(theta-max(v[k],0))*dt+xi*np.sqrt(max(v[k],0))*W_rho_acc[k]
     elif pricer.sch=="milstein":
         for k in range(n-1):
-            v[k+1]=v[k]+kappa*(theta-max(v[k],0))*dt+xi*np.sqrt(max(v[k],0))*W_rho_acc[k]+0.25*xi*xi*dt*(W_rho_acc[k]*W_rho_acc[k]-1)
+            v[k+1]=v[k]+kappa*(theta-max(v[k],0))*dt+xi*np.sqrt(max(v[k],0))*W_rho_acc[k]+0.25*xi*xi*(W_rho_acc[k]*W_rho_acc[k]-dt)
     return v
 
-######### OptionPricer class ##########
+######### OptionPricer CLASS ##########
 
 class optionPricer():
     def __init__(self,S0,K,sigma,T,model,option_type,sch,order="call",r=np.log(1.1),**kwargs):
@@ -85,13 +85,18 @@ class optionPricer():
             self.rho=kwargs["rho"]
             self.v0=kwargs["v0"]
     def BlackScholes(self):
-        d1=(np.log(self.S0/self.strike)+(self.rate+0.5*self.vol**2)*self.maturity)/(self.vol*np.sqrt(self.maturity))
-        d2=d1-self.vol*np.sqrt(self.maturity)
-        if self.order=="call":
-            return self.S0*ss.norm.cdf(d1)-self.strike*np.exp(-self.rate*self.maturity)*ss.norm.cdf(d2)
-        if self.order=="put":
-            return self.strike*np.exp(-self.rate*self.maturity)*ss.norm.cdf(-d2)-self.S0*ss.norm.cdf(-d1)
+        """ Utilise la formule de BlackScholes pour déterminer le prix"""
+        if self.option_type=="E":
+            d1=(np.log(self.S0/self.strike)+(self.rate+0.5*self.vol**2)*self.maturity)/(self.vol*np.sqrt(self.maturity))
+            d2=d1-self.vol*np.sqrt(self.maturity)
+            if self.order=="call":
+                return self.S0*ss.norm.cdf(d1)-self.strike*np.exp(-self.rate*self.maturity)*ss.norm.cdf(d2)
+            if self.order=="put":
+                return self.strike*np.exp(-self.rate*self.maturity)*ss.norm.cdf(-d2)-self.S0*ss.norm.cdf(-d1)
+        else:
+            raise AttributeError('Cannot use "BlackScholes" method for Asian options')
     def schema(self,S,dt,W,*v):
+        """ Applique une itération du schéma numérique pour l'option considérée"""
         if self.sch=="euler":   
             """ pour l instant, on ne considère que des options européennes """
             if self.model=="BS":
@@ -104,7 +109,7 @@ class optionPricer():
             elif self.model=="Heston":
                 return S*np.exp((self.rate-0.5*v[0])*dt+np.sqrt(v[0])*W)
     def payoff(self,S):
-        """ S est un numpy.array """
+        """ Calcule le payoff de l'option selon l'ordre de celle-ci (call ou put) """
         if self.order=="call":
             return max(S-self.strike,0)
         elif self.order=="put":
@@ -112,19 +117,27 @@ class optionPricer():
         else:
             raise TypeError("Unvalid 'order' attribute. Must be either 'call' or 'put'.")
     def mc_explicit(self,N,pool):
-        # on garde les possibilités de paralléliser ou non pour les comparer
-        if pool==None:
-            S=np.zeros(N)
-            W=np.random.normal(0,np.sqrt(self.maturity),N)
-            for _ in range(N):
-                S[_]=self.S0*np.exp((self.rate-0.5*self.vol**2)*self.maturity+self.vol*W[_])
+        """ Calcule le prix par une méthode de monte-Carlo classique, à partir de la formule explicite.
+        N'est utilisable que dans le modèle de BlackScholes"""
+        if self.model=="BS":
+            # on garde les possibilités de paralléliser ou non pour les comparer
+            if pool==None:
+                S=np.zeros(N)
+                W=np.random.normal(0,np.sqrt(self.maturity),N)
+                for _ in range(N):
+                    S[_]=self.S0*np.exp((self.rate-0.5*self.vol**2)*self.maturity+self.vol*W[_])
+            else:
+                future_res=[pool.apply_async(MC_simulation(0),(self,)) for _ in range(N)]
+                S=np.array([f.get() for f in future_res])
+                return np.exp(-self.rate*self.maturity)*np.mean(list(map(lambda x: self.payoff(x),S)))
         else:
-            future_res=[pool.apply_async(MC_simulation(0),(self,)) for _ in range(N)]
-            S=np.array([f.get() for f in future_res])
-            return np.exp(-self.rate*self.maturity)*np.mean(list(map(lambda x: self.payoff(x),S)))
+            raise AttributeError("Cannot use 'mc_explicit' method if 'model' attribute different to 'BS'")
     def calcul_BS(self,t,W):
         return(self.S0*np.exp((self.rate-0.5*self.vol**2)*t+self.vol*np.sqrt(t)*W))
     def mc(self,n,N,pool=None):
+        """ Calcule le prix avec un schema de discrétisation de l'EDS, puis par une méthode de Monte-Carlo classique (1 niveau)
+        """
+        # si on ne parallélise pas
         if pool==None:
             dt=self.maturity/n
             Payoff=0
@@ -154,8 +167,9 @@ class optionPricer():
             S=np.array([f.get() for f in future_res])
             return np.exp(-self.rate*self.maturity)*np.mean(list(map(lambda x: self.payoff(x),S)))
     def mlmc(self,n,m):
+        """ Calcule le prix par méthode multilevel de Monte-Carlo"""
         L=int(np.log(n)/np.log(m))
-        # l=0
+        # Premier niveau
         if self.option_type=="E":
             N0=int((m-1)*self.maturity*(n**2)*L)
         elif self.option_type=="A":
@@ -175,13 +189,11 @@ class optionPricer():
                 for j in range(N0):
                     S_N0=0
                     S_N0+=(1/2)*(self.S0+self.calcul_BS(self.maturity,W_N0[j]))
-                    Payoff+=self.payoff(S_N0)     
+                    Payoff+=self.payoff(S_N0) 
         Payoff/=N0
-        # for l>1
-        # inverser le role de n et L, c est n qui donne L
+
+        # Niveaux supérieurs
         for l in range(1,L):
-            #choix de Nl
-            # déterminons la variance de Pl-Pl-1 puis choix de Nl
             Nl=0
             if self.option_type=="E":
                 Nl=int(N0/(m**l))
@@ -201,7 +213,9 @@ class optionPricer():
 
                 for i in range(m**(l-1)):
                     W_coarse[i+1]=W_thin[(i+1)*m]
-                dt_coarse=self.maturity/(m**(l-1))
+                dt_coarse=dt_thin*m
+
+                # dans lecas du modèle de Heston
                 if self.model=="Heston":
                     v_thin=build_Cox_Ross(W_thin,self.rho,self.kappa,self.theta,self.xi,dt_thin,self)
                     v_coarse=build_Cox_Ross(W_coarse,self.rho,self.kappa,self.theta,self.xi,dt_coarse,self)
@@ -209,14 +223,19 @@ class optionPricer():
                         S_thin=self.schema(S_thin,dt_thin,W_thin[i+1]-W_thin[i],max(v_thin[i],0))
                         if i%m==0:
                             S_coarse=self.schema(S_coarse,dt_coarse,W_coarse[i//m+1]-W_coarse[i//m],max(v_coarse[i//m],0))
+                # dans le cas du modèle de BS
                 elif self.model=="BS":
+                    # si on a une option européenne
                     if self.option_type=="E":
                         W_ind=np.random.normal(0,np.sqrt(dt_thin),m**l)
+                        W_ind_coarse=np.random.normal(0,np.sqrt(dt_coarse),m**(l-1))
                         for i in range(m**l):
                             S_thin=self.schema(S_thin,dt_thin,W_ind[i])
                             if i%m==0:
-                                S_coarse=self.schema(S_coarse,dt_coarse,np.random.normal(0,np.sqrt(dt_coarse)))
+                                S_coarse=self.schema(S_coarse,dt_coarse,W_ind_coarse[i//m])
+                    # si on a une option asiatique
                     elif self.option_type=="A":
+                        # dans ce cas, S_thin, S_coarse désigne les intégrales et non le prix du sous jacent
                         S_thin=0
                         S_coarse=0
                         for i in range(m**l):
@@ -225,10 +244,11 @@ class optionPricer():
                                 S_coarse+=(dt_coarse/(2*self.maturity))*(self.calcul_BS((i//m)*dt_coarse,W_coarse[i//m])+self.calcul_BS((i//m+1)*dt_coarse,W_coarse[i//m+1]))
                     else:
                         raise AttributeError("'option_type' attribute must be either 'E' (european) or 'A' (asian).")
-                                
                 Payoff+=(self.payoff(S_thin)-self.payoff(S_coarse))/Nl
+
         return np.exp(-self.rate*self.maturity)*Payoff
-    def price(self,order,method="mc_e",pool=None,m=5,N=10**3,n=50,alpha=0.05):
+    def price(self,order,method,m,N,n,pool=None,alpha=0.05):
+        """ Retourne le prix et le temps de calcul selon la méthode utilisée"""
         self.order=order
         if method=="bs":
             start=time()
@@ -248,6 +268,7 @@ class optionPricer():
             return payoff,time()-start
 
     def confidence_interval(self,alpha,payoffs):
+        """ Retourne un intervalle de confiance de niveau alpha """
         n=payoffs.shape[0]
         V=payoffs.std()**2
         mu=payoffs.mean()
@@ -258,26 +279,40 @@ class optionPricer():
             return [mu-ss.norm.cdf(1-alpha/2)*np.sqrt(V/n),
                     mu+ss.norm.cdf(1-alpha/2)*np.sqrt(V/n)]
 
-    def delta(self,order,method,epsilon=1e-2):
-        base_price=self.price(order,method)[0]
+    def delta(self,order,method,m,N,n,epsilon=1e-2):
+        base_price=self.price(order,method,m,N,n)[0]
         self.S0+=epsilon
-        new_price=self.price(order,method)[0]
+        new_price=self.price(order,method,m,N,n)[0]
         delta=(new_price-base_price)/epsilon
 
         self.S0-=epsilon
         return delta
 
-    def gamma(self,order,method,epsilon=1e-2):
-        base_delta=self.delta(order,method)
+    def gamma(self,order,method,m,N,n,epsilon=1e-2):
+        base_delta=self.delta(order,method,m,N,n)
         self.S0+=epsilon
-        new_delta=self.delta(order,method)
+        new_delta=self.delta(order,method,m,N,n)
         gamma=(new_delta-base_delta)/epsilon
 
         return gamma
 
 if __name__=="__main__":
-    o=optionPricer(100,100,0.2,3,"BS","A","euler",kappa=3,rho=0,xi=0.2,theta=.9,v0=0.1)
-    #print("reference price: {}".format(o.price("call",'bs')))
-    print("MC euler price: {}".format(o.price("call","mc")))
-    print('MLMC price with Heston model: {}'.format(o.price("call","mlmc")))
+    # INPUTS
+    order="call"    # call, put
+    S0=100
+    strike=100
+    rate=np.log(1.1)
+    vol=0.2
+    maturity=3
+    N=1000  # nb d 'échantillons pour  MonteCarlo classique avec schema numérique
+    n=50    # nb de pas de disrétisation pour schéma numérique 
+    m=2     # détermine le nombre de niveau par L=log(n)/log(m)
+    model="BS"  # BS: Black-Scholes, Heston: Heston
+    sch="euler" # euler, milstein
+    option_type="A" # E: european, A: asian
+
+    o=optionPricer(S0,strike,vol,maturity,model,option_type,sch,r=rate,kappa=3,rho=0,xi=0.2,theta=.9,v0=0.1)
+    
+    print("MC price: {}".format(o.price(order,"mc",m,N,n)))
+    print('MLMC price: {}'.format(o.price(order,"mlmc",m,N,n)))
 
