11c5f25e0054405add9dea182d1d9afcde49c5de
Rayan <rayans0203@gmail.com>
2018-02-05 17:52:29 +0100

    Added resources.html url

--- website/optionPricer/userInterface/urls.py
+++ website/optionPricer/userInterface/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 	url(r'home/$',views.home,name='home'),
 	url(r'pricer/$',views.pricer,name='pricer'),
+	url(r'resources$',views.resources,name='resources'),
 	url(r'computation/'+r'([0-9]*[.]?[0-9]+)/'*5+r'(\w+)/'*3,views.computation,name="computation"),
 	url(r'faq/$',ListView.as_view(queryset=models.Post.objects.all(),
 		template_name="userInterface/faq.html")),
